76188f86dd49df4a34d09a501522d95c12618be9
naphattharawat <naphattharawat@gmail.com>
2019-02-11 08:38:14 +0700

    fix bug port +

--- src/app.ts
+++ src/app.ts
@@ -91,7 +91,7 @@ let checkAuth = (req, res, next) => {
 
 let dbConnection: MySqlConnectionConfig = {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: +process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
