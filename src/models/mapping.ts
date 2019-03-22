096dd7227a58a9935cc56660cbee2460f9381b23
naphattharawat <naphattharawat@gmail.com>
2019-03-22 13:59:10 +0700

    update edi

--- src/models/mapping.ts
+++ src/models/mapping.ts
@@ -1,18 +1,28 @@
 import Knex = require('knex');
 
 export class MappingModel {
-  getEdiLabelerCode(knex: Knex, productId) {
+  getMappings(knex: Knex, productId) {
     return knex('mm_products as p')
-      .select('edi_labeler_code')
+      .select('edi_labeler_code', 'tmt_id')
       .where({ 'product_id': productId })
   }
 
-  saveEdiLabelerCode(knex: Knex, productId, ediLabelerCode) {
+  saveMappings(knex: Knex, productId, data) {
     return knex('mm_products as p')
-      .update({ 'edi_labeler_code': ediLabelerCode })
+      .update(data)
       .where({ 'product_id': productId })
   }
 
 
+  searchTmtCode(knex: Knex, query: any) {
+    let _query = `%${query}%`
+    return knex('tmt_tpu as tpu')
+      .select('tpu.TMTID', 'tpu.FSN')
+      .where(w => {
+        w.where('tpu.TMTID', 'like', _query)
+          .orWhere('tpu.FSN', 'like', _query)
+      })
+      .limit(10);
+  }
 
 }
--- src/routes/mappings.ts
+++ src/routes/mappings.ts
@@ -8,13 +8,13 @@ const router = express.Router();
 
 const mappingModel = new MappingModel();
 
-router.get('/edi-labeler-code', wrap(async (req, res, next) => {
+router.get('/', wrap(async (req, res, next) => {
   const db = req.db;
   const productId = req.query.productId;
 
   try {
-    const results = await mappingModel.getEdiLabelerCode(db, productId)
-    res.send({ ok: true, rows: results[0].edi_labeler_code });
+    const results = await mappingModel.getMappings(db, productId)
+    res.send({ ok: true, rows: results[0] });
   } catch (error) {
     res.send({ ok: false, error: error.message });
   } finally {
@@ -22,13 +22,13 @@ router.get('/edi-labeler-code', wrap(async (req, res, next) => {
   }
 }));
 
-router.post('/edi-labeler-code', wrap(async (req, res, next) => {
+router.post('/', wrap(async (req, res, next) => {
   const db = req.db;
   const productId = req.body.productId;
-  const ediLabelerCode = req.body.ediLabelerCode;
+  const data = req.body.data;
 
   try {
-    const results = await mappingModel.saveEdiLabelerCode(db, productId, ediLabelerCode)
+    const results = await mappingModel.saveMappings(db, productId, data)
     res.send({ ok: true });
   } catch (error) {
     res.send({ ok: false, error: error.message });
@@ -37,4 +37,33 @@ router.post('/edi-labeler-code', wrap(async (req, res, next) => {
   }
 }));
 
+
+
+router.get('/search/tmt-code', async (req, res, next) => {
+
+  let db = req.db;
+  const query = req.query.q;
+
+  try {
+    let rs: any = await mappingModel.searchTmtCode(db, query);
+    if (rs.length) {
+      let items = [];
+      rs.forEach(v => {
+        let obj: any = {};
+        obj.fsn = v.FSN;
+        obj.tmtid = v.TMTID;
+        items.push(obj);
+      });
+      res.send(items);
+    } else {
+      res.send([]);
+    }
+  } catch (error) {
+    console.log(error);
+    res.send([]);
+  } finally {
+    db.destroy();
+  }
+
+});
 export default router;
