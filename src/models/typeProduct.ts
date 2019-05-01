20e78bd768263d9f27c33d34ed211f490bf8a10c
naphattharawat <naphattharawat@gmail.com>
2019-05-01 23:44:34 +0700

    generic เพิ่ม หมวดสินค้าย่อย และเพิ่มเมนู การผูกหมวดสินค้า กับหมวดสินค้าย่อย

--- src/models/typeProduct.ts
+++ src/models/typeProduct.ts
@@ -10,8 +10,57 @@ export class typeProductModel {
     return query
   }
 
+  getGenericTypeLV1(knex: Knex, deleted: boolean) {
+    let query = knex('mm_generic_types')
+    if (!deleted) {
+      query.where('is_deleted', 'N')
+    }
+    return query
+  }
+
+  getGenericTypeLV2(knex: Knex, deleted: boolean) {
+    let query = knex('mm_generic_types_lv2 as mg2')
+      .select('mg.generic_type_name as generic_type_lv1_name', 'mg2.generic_type_lv1_id',
+        'mg2.generic_type_lv2_id', 'mg2.generic_type_lv2_name', 'mg2.is_deleted')
+      .join('mm_generic_types as mg', 'mg.generic_type_id', 'mg2.generic_type_lv1_id')
+    if (!deleted) {
+      query.where('mg2.is_deleted', 'N')
+    }
+    console.log(query.toString());
+
+    return query
+  }
+
+  getGenericTypeLV3(knex: Knex, deleted: boolean) {
+    let query = knex('mm_generic_types_lv3 as mg3')
+      .select('mg.generic_type_name as generic_type_lv1_name', 'mg2.generic_type_lv1_id',
+        'mg2.generic_type_lv2_id', 'mg2.generic_type_lv2_name',
+        'mg3.generic_type_lv3_id', 'mg3.generic_type_lv3_name', 'mg3.is_deleted')
+      .join('mm_generic_types as mg', 'mg.generic_type_id', 'mg3.generic_type_lv1_id')
+      .join('mm_generic_types_lv2 as mg2', 'mg2.generic_type_lv2_id', 'mg3.generic_type_lv2_id')
+    if (!deleted) {
+      query.where('mg3.is_deleted', 'N')
+    }
+    return query
+  }
+
   save(knex: Knex, datas: any) {
     return knex('mm_generic_types')
+      .insert(datas);
+  }
+
+  saveLv1(knex: Knex, datas: any) {
+    return knex('mm_generic_types')
+      .insert(datas);
+  }
+
+  saveLv2(knex: Knex, datas: any) {
+    return knex('mm_generic_types_lv2')
+      .insert(datas);
+  }
+
+  saveLv3(knex: Knex, datas: any) {
+    return knex('mm_generic_types_lv3')
       .insert(datas);
   }
 
@@ -20,20 +69,60 @@ export class typeProductModel {
       .where('generic_type_id', typeId)
       .update(datas);
   }
+  updateLV1(knex: Knex, typeId: string, datas: any) {
+    return knex('mm_generic_types')
+      .where('generic_type_id', typeId)
+      .update(datas);
+  }
+  updateLV2(knex: Knex, genericTypeLV2Id: string, datas: any) {
+    return knex('mm_generic_types_lv2')
+      .where('generic_type_lv2_id', genericTypeLV2Id)
+      .update(datas);
+  }
+  updateLV3(knex: Knex, genericTypeLV2I3: string, datas: any) {
+    return knex('mm_generic_types_lv3')
+      .where('generic_type_lv3_id', genericTypeLV2I3)
+      .update(datas);
+  }
 
   detail(knex: Knex, typeId: string) {
     return knex('mm_generic_types')
       .where('generic_type_id', typeId);
   }
 
-  remove(knex: Knex, typeId: string) {
+  removeLV1(knex: Knex, typeId: string) {
     return knex('mm_generic_types')
       .where('generic_type_id', typeId)
       .update('is_deleted', 'Y')
   }
-  returnRemove(knex: Knex, Id: string) {
+
+  removeLV2(knex: Knex, typeId: string) {
+    return knex('mm_generic_types_lv2')
+      .where('generic_type_lv2_id', typeId)
+      .update('is_deleted', 'Y')
+  }
+
+  removeLV3(knex: Knex, typeId: string) {
+    return knex('mm_generic_types_lv3')
+      .where('generic_type_lv3_id', typeId)
+      .update('is_deleted', 'Y')
+  }
+
+  returnRemoveLV1(knex: Knex, Id: string) {
     return knex('mm_generic_types')
       .where('generic_type_id', Id)
+      .update('is_deleted', 'N');
+  }
+
+  returnRemoveLV2(knex: Knex, Id: string) {
+    return knex('mm_generic_types_lv2')
+      .where('generic_type_lv2_id', Id)
+      .update('is_deleted', 'N');
+  }
+
+  returnRemoveLV3(knex: Knex, Id: string) {
+    return knex('mm_generic_types_lv3')
+      .where('generic_type_lv3_id', Id)
       .update('is_deleted', 'N');
   }
 }
--- src/routes/typeProduct.ts
+++ src/routes/typeProduct.ts
@@ -23,11 +23,56 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/return', (req, res, next) => {
+router.get('/lv1', (req, res, next) => {
+  let db = req.db;
+  const deleted = req.query.deleted == 'false' ? false : true;
+  typeProduct.getGenericTypeLV1(db, deleted)
+    .then((results: any) => {
+      res.send({ ok: true, rows: results });
+    })
+    .catch(error => {
+      res.send({ ok: false, error: error })
+    })
+    .finally(() => {
+      db.destroy();
+    });
+});
+
+router.get('/lv2', (req, res, next) => {
+  let db = req.db;
+  const deleted = req.query.deleted == 'false' ? false : true;
+  typeProduct.getGenericTypeLV2(db, deleted)
+    .then((results: any) => {
+      res.send({ ok: true, rows: results });
+    })
+    .catch(error => {
+      res.send({ ok: false, error: error })
+    })
+    .finally(() => {
+      db.destroy();
+    });
+});
+
+router.get('/lv3', (req, res, next) => {
+  let db = req.db;
+  const deleted = req.query.deleted == 'false' ? false : true;
+  typeProduct.getGenericTypeLV3(db, deleted)
+    .then((results: any) => {
+      res.send({ ok: true, rows: results });
+    })
+    .catch(error => {
+      res.send({ ok: false, error: error })
+    })
+    .finally(() => {
+      db.destroy();
+    });
+});
+
+router.post('/return/lv1', (req, res, next) => {
   let typeId = req.body.id;
   let db = req.db;
 
-  typeProduct.returnRemove(db, typeId)
+  typeProduct.returnRemoveLV1(db, typeId)
     .then((results: any) => {
       res.send({ ok: true })
     })
@@ -38,6 +83,37 @@ router.post('/return', (req, res, next) => {
       db.destroy();
     });
 });
+router.post('/return/lv2', (req, res, next) => {
+  let typeId = req.body.id;
+  let db = req.db;
+
+  typeProduct.returnRemoveLV2(db, typeId)
+    .then((results: any) => {
+      res.send({ ok: true })
+    })
+    .catch(error => {
+      res.send({ ok: false, error: error })
+    })
+    .finally(() => {
+      db.destroy();
+    });
+});
+router.post('/return/lv3', (req, res, next) => {
+  let typeId = req.body.id;
+  let db = req.db;
+
+  typeProduct.returnRemoveLV3(db, typeId)
+    .then((results: any) => {
+      res.send({ ok: true })
+    })
+    .catch(error => {
+      res.send({ ok: false, error: error })
+    })
+    .finally(() => {
+      db.destroy();
+    });
+});
+
 router.post('/', (req, res, next) => {
   let typeName = req.body.typeName;
   let prefixName = req.body.prefixName;
@@ -94,6 +170,178 @@ router.put('/:typeId', (req, res, next) => {
   }
 });
 
+
+router.post('/lv1', (req, res, next) => {
+  let genericTypeLV1Name = req.body.genericTypeLV1Name;
+  let prefixName = req.body.prefixName;
+
+  let db = req.db;
+
+  if (genericTypeLV1Name) {
+    let datas: any = {
+      generic_type_name: genericTypeLV1Name,
+      prefix_name: prefixName,
+      prefix_no: 1
+    }
+
+    typeProduct.saveLv1(db, datas)
+      .then((results: any) => {
+        res.send({ ok: true })
+      })
+      .catch(error => {
+        res.send({ ok: false, error: error })
+      })
+      .finally(() => {
+        db.destroy();
+      });
+  } else {
+    res.send({ ok: false, error: 'ข้อมูลไม่สมบูรณ์' });
+  }
+});
+
+router.put('/lv1/:genericTypeLV1Id', (req, res, next) => {
+  let genericTypeLV1Id = req.params.genericTypeLV1Id;
+  let genericTypeLV1Name = req.body.genericTypeLV1Name;
+  let prefixName = req.body.prefixName;
+
+  let db = req.db;
+
+  if (genericTypeLV1Id) {
+    let datas: any = {
+      generic_type_name: genericTypeLV1Name,
+      prefix_name: prefixName
+    }
+
+
+    typeProduct.updateLV1(db, genericTypeLV1Id, datas)
+      .then((results: any) => {
+        res.send({ ok: true })
+      })
+      .catch(error => {
+        res.send({ ok: false, error: error })
+      })
+      .finally(() => {
+        db.destroy();
+      });
+  } else {
+    res.send({ ok: false, error: 'ข้อมูลไม่สมบูรณ์' });
+  }
+});
+
+router.post('/lv2', (req, res, next) => {
+  let genericTypeLV2Name = req.body.genericTypeLV2Name;
+  let genericTypeLV1Id = req.body.genericTypeLV1Id;
+
+  let db = req.db;
+
+  if (genericTypeLV2Name) {
+    let datas: any = {
+      generic_type_lv2_name: genericTypeLV2Name,
+      generic_type_lv1_id: genericTypeLV1Id
+    }
+
+    typeProduct.saveLv2(db, datas)
+      .then((results: any) => {
+        res.send({ ok: true })
+      })
+      .catch(error => {
+        res.send({ ok: false, error: error })
+      })
+      .finally(() => {
+        db.destroy();
+      });
+  } else {
+    res.send({ ok: false, error: 'ข้อมูลไม่สมบูรณ์' });
+  }
+});
+
+router.put('/lv2/:genericTypeLV2Id', (req, res, next) => {
+  let genericTypeLV2Id = req.params.genericTypeLV2Id;
+  let genericTypeLV1Id = req.body.genericTypeLV1Id;
+  let genericTypeLV2Name = req.body.genericTypeLV2Name;
+
+  let db = req.db;
+
+  if (genericTypeLV2Id) {
+    let datas: any = {
+      generic_type_lv2_name: genericTypeLV2Name,
+      generic_type_lv1_id: genericTypeLV1Id
+    }
+
+    typeProduct.updateLV2(db, genericTypeLV2Id, datas)
+      .then((results: any) => {
+        res.send({ ok: true })
+      })
+      .catch(error => {
+        res.send({ ok: false, error: error })
+      })
+      .finally(() => {
+        db.destroy();
+      });
+  } else {
+    res.send({ ok: false, error: 'ข้อมูลไม่สมบูรณ์' });
+  }
+});
+
+router.post('/lv3', (req, res, next) => {
+  let genericTypeLV3Name = req.body.genericTypeLV3Name;
+  let genericTypeLV1Id = req.body.genericTypeLV1Id;
+  let genericTypeLV2Id = req.body.genericTypeLV2Id;
+
+  let db = req.db;
+
+  if (genericTypeLV3Name) {
+    let datas: any = {
+      generic_type_lv3_name: genericTypeLV3Name,
+      generic_type_lv2_id: genericTypeLV2Id,
+      generic_type_lv1_id: genericTypeLV1Id
+    }
+
+    typeProduct.saveLv3(db, datas)
+      .then((results: any) => {
+        res.send({ ok: true })
+      })
+      .catch(error => {
+        res.send({ ok: false, error: error })
+      })
+      .finally(() => {
+        db.destroy();
+      });
+  } else {
+    res.send({ ok: false, error: 'ข้อมูลไม่สมบูรณ์' });
+  }
+});
+
+router.put('/lv3/:genericTypeLV3Id', (req, res, next) => {
+  let genericTypeLV3Id = req.params.genericTypeLV3Id;
+  let genericTypeLV1Id = req.body.genericTypeLV1Id;
+  let genericTypeLV2Id = req.body.genericTypeLV2Id;
+  let genericTypeLV3Name = req.body.genericTypeLV3Name;
+
+  let db = req.db;
+
+  if (genericTypeLV3Id) {
+    let datas: any = {
+      generic_type_lv3_name: genericTypeLV3Name,
+      generic_type_lv1_id: genericTypeLV1Id,
+      generic_type_lv2_id: genericTypeLV2Id
+    }
+
+    typeProduct.updateLV3(db, genericTypeLV3Id, datas)
+      .then((results: any) => {
+        res.send({ ok: true })
+      })
+      .catch(error => {
+        res.send({ ok: false, error: error })
+      })
+      .finally(() => {
+        db.destroy();
+      });
+  } else {
+    res.send({ ok: false, error: 'ข้อมูลไม่สมบูรณ์' });
+  }
+});
+
 router.get('/detail/:typeId', (req, res, next) => {
   let typeId = req.params.typeId;
   let db = req.db;
@@ -110,11 +358,43 @@ router.get('/detail/:typeId', (req, res, next) => {
     });
 });
 
-router.delete('/:typeId', (req, res, next) => {
+router.delete('/lv1/:typeId', (req, res, next) => {
   let typeId = req.params.typeId;
   let db = req.db;
 
-  typeProduct.remove(db, typeId)
+  typeProduct.removeLV1(db, typeId)
+    .then((results: any) => {
+      res.send({ ok: true })
+    })
+    .catch(error => {
+      res.send({ ok: false, error: error })
+    })
+    .finally(() => {
+      db.destroy();
+    });
+});
+
+router.delete('/lv2/:typeId', (req, res, next) => {
+  let typeId = req.params.typeId;
+  let db = req.db;
+
+  typeProduct.removeLV2(db, typeId)
+    .then((results: any) => {
+      res.send({ ok: true })
+    })
+    .catch(error => {
+      res.send({ ok: false, error: error })
+    })
+    .finally(() => {
+      db.destroy();
+    });
+});
+
+router.delete('/lv3/:typeId', (req, res, next) => {
+  let typeId = req.params.typeId;
+  let db = req.db;
+
+  typeProduct.removeLV3(db, typeId)
     .then((results: any) => {
       res.send({ ok: true })
     })
